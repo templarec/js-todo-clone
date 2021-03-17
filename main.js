2ae4f0b29468e56c48657cac171d8c5fd2edbfe8
Lorenzo Bernini <templarec@gmail.com>
2021-03-17 20:35:54 +0100

    Inserimento ok

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
+	var button = $('.to-do [type="button"]');
 
+	button.click(function () {
+		var todo = $('#todo');
+		var inputTxt = $('#todoTitle');
+		var testo = inputTxt.val();
+		var templateLi = $('#template li');
+		var template = templateLi.clone();
+		template.children('span#titolo').text(testo);
+		todo.append(template);
+	});
 });
